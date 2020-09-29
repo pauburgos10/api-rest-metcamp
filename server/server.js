1322d3f7e72efff6a8e1e37fb1821e6a407f086d
Paula Burgos <paula.burgos@valtira.net>
2020-09-28 21:57:15 -0300

    simplificar movies

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+var movies_result = movies['Search'];
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,41 +14,40 @@ app.use(bodyParser.json())
 
 
 app.get('/peliculas', function (req, res) {
-  res.json(movies['Search'])
+  res.json(movies_result)
 })
 
 app.get('/peliculas/:id', function (req, res) {
-  const movie = movies['Search'].filter(m => m['imdbID'] == req.params.id)
+  const movie = movies_result.filter(m => m['imdbID'] == req.params.id)
   res.json(movie)
 })
 
 app.post('/peliculas', function (req, res) {
-  movies['Search'].push(req.body)
-  res.json(movies['Search'][movies['Search'].length -1])
+  movies_result.push(req.body)
+  res.json(movies_result[movies_result.length -1])
 })
 
 app.put('/peliculas', function (req, res) {
   var toUpdate = req.body
   var index = -1
-  for (var i in movies['Search']) {
-    if (movies['Search'][i]['imdbID'] == toUpdate['imdbID']) {
-      movies['Search'][i].Title = toUpdate['Title'];
-      movies['Search'][i].Type = toUpdate['Type'];
-      movies['Search'][i].Year = toUpdate['Year'];
-      movies['Search'][i].Poster = toUpdate['Poster'];
+  for (var i in movies_result) {
+    if (movies_result[i]['imdbID'] == toUpdate['imdbID']) {
+      movies_result[i].Title = toUpdate['Title'];
+      movies_result[i].Type = toUpdate['Type'];
+      movies_result[i].Year = toUpdate['Year'];
+      movies_result[i].Poster = toUpdate['Poster'];
       index = i
       break; //Encontramos la peli. Ya no seguimos buscando!
     }
   }
-  res.json(movies['Search'][index])
+  res.json(movies_result[index])
 })
 
 app.delete('/peliculas/:id', function (req, res) {
   var toDelete = req.params.id
   var index = -1
-  index = movies['Search'].findIndex(m => m['imdbID'] == toDelete)
-  //console.log('index =', index)
-  movies['Search'].splice(index,1)
+  index = movies_result.findIndex(m => m['imdbID'] == toDelete)
+  movies_result.splice(index,1)
   res.json('peli borrada pelicula'+ index)
 })
 
